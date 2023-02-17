7809d1b8eac11a6f896df45554672aa1e15fb2b7
Kinshu1996 <ankitagrawal678y@gmail.com>
2023-02-17 17:29:18 +0530

    use join method in app file

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import '../styles/App.css';
 const arr = JSON.parse(window.localStorage.getItem('props') || `["hello","world"]`) // do not change
 const Join = (props) =>{
+  
   return(
     <div id ="join">
-      {/* Access prop 'words' and print it using .join like words.join(',')*/
-      props.join(',')
-      }
+      {/* Access prop 'words' and print it using .join like words.join(',')*/}
+      {props.words.join(',')}
     </div>
   )
 }
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div id="main">
-      <Join  />
+      <Join words={arr}/>
     </div>
   )
 }
